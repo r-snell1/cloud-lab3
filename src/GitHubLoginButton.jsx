93a64bca4df0adf7b382970d951f4d783f1105a9
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2024-12-05 16:31:37 -0500

    Update GitHubLoginButton.jsx

--- src/GitHubLoginButton.jsx
+++ src/GitHubLoginButton.jsx
@@ -4,7 +4,7 @@ const GitHubLoginButton = () => {
   const handleLogin = () => {
     // GitHub OAuth URL with your Client ID
     const clientId = 'YOUR_GITHUB_CLIENT_ID';
-    const redirectUri = 'http://localhost:3000/auth/callback'; // This should be your app's callback URL
+    const redirectUri = 'https://green-ocean-0e709031e.4.azurestaticapps.net'; // This should be your app's callback URL
     const scope = 'read:user'; // You can adjust the scope based on what you need
     const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
     
